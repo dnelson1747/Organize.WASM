f0a45001267789cde0de228c8230ea7fa09868ab
David Nelson <dnelson1747@gmail.com>
2021-03-14 17:59:21 -0700

    Add project files.

---
+++ Organize.WASM/Pages/SignBase.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Organize.WASM.Pages
+{
+    public class SignBase
+    {
+    }
+}
---
+++ Organize.WASM/Pages/SignIn.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Organize.WASM.Pages
+{
+    public partial class SignIn
+    {
+        private string UserName { get; set; }
+    }
+}
