93f5855e9b647d1cdc0273f61e7fde48316b8756
David Nelson <dnelson1747@gmail.com>
2021-03-15 18:12:25 -0700

    .36

---
+++ Organize.WASM/Pages/SignInBase.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.AspNetCore.Components;
+using Organize.Shared.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Organize.WASM.Pages
+{
+    public class SignInBase : ComponentBase
+    {
+
+        protected string Day { get; } = DateTime.Now.DayOfWeek.ToString();
+        protected string Username { get; set; } = "Dave";
+        protected User User { get; set; } = new User();
+
+        protected void HandleUserNameChanged(ChangeEventArgs eventArgs)
+        {
+            Username = eventArgs.Value.ToString();
+        }
+
+        protected void HandleUserNameValueChanged(string value)
+        {
+            Username = value;
+        }
+
+    }
+}
+
