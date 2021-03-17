e87e9fdde7f0738634269b3c3c9e6acbd23a05fe
David Nelson <dnelson1747@gmail.com>
2021-03-16 20:10:02 -0700

    .40

--- Organize.WASM/Controls/ValidationInputBase.cs
+++ Organize.WASM/Controls/ValidationInputBase.cs
@@ -16,6 +16,10 @@ namespace Organize.WASM.Controls
 
         [Parameter]
         public string Error { get; set; }
+
+        [Parameter(CaptureUnmatchedValues = true)]
+        public Dictionary<string, object> InputAttributes { get; set; }
+
         protected async void HandleInputChanged(ChangeEventArgs eventArgs)
         {
           await ValueChanged.InvokeAsync(eventArgs.Value.ToString());
