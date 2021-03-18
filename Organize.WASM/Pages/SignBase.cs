b8dac4ebb05e4bc0f4e19f20323b90ab44af0a40
dnelson1747 <dnelson1747@gmail.com>
2021-03-18 11:24:05 -0700

    .42

---
+++ Organize.WASM/Pages/SignBase.cs
@@ -0,0 +1,44 @@
+﻿using Microsoft.AspNetCore.Components;
+using Microsoft.AspNetCore.Components.Forms;
+using Organize.Shared.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Linq.Expressions;
+using System.Threading.Tasks;
+
+namespace Organize.WASM.Pages
+{
+    public class SignBase : ComponentBase
+    {
+        //protected string Username { get; set; } = "Dave";
+        protected User User { get; set; } = new User();
+
+        protected EditContext EditContext { get; set; }
+
+        protected override void OnInitialized()
+        {
+            base.OnInitialized();
+            EditContext = new EditContext(User);
+        }
+
+        public string GetError(Expression<Func<object>> fu)
+        {
+            if (EditContext == null)
+            {
+                return null;
+            }
+            return EditContext.GetValidationMessages(fu).FirstOrDefault();
+        }
+
+        //protected void HandleUserNameChanged(ChangeEventArgs eventArgs)
+        //{
+        //    Username = eventArgs.Value.ToString();
+        //}
+
+        //protected void HandleUserNameValueChanged(string value)
+        //{
+        //    Username = value;
+        //}
+    }
+}
--- Organize.WASM/Pages/SignInBase.cs
+++ Organize.WASM/Pages/SignInBase.cs
@@ -9,39 +9,39 @@ using System.Threading.Tasks;
 
 namespace Organize.WASM.Pages
 {
-    public class SignInBase : ComponentBase
+    public class SignInBase : SignBase
     {
 
         protected string Day { get; } = DateTime.Now.DayOfWeek.ToString();
-        protected string Username { get; set; } = "Dave";
-        protected User User { get; set; } = new User();
+        //protected string Username { get; set; } = "Dave";
+        //protected User User { get; set; } = new User();
 
-        protected EditContext EditContext { get; set; }
+        //protected EditContext EditContext { get; set; }
 
-        protected override void OnInitialized()
-        {
-            base.OnInitialized();
-            EditContext = new EditContext(User);
-        }
+        //protected override void OnInitialized()
+        //{
+        //    base.OnInitialized();
+        //    EditContext = new EditContext(User);
+        //}
 
-        protected void HandleUserNameChanged(ChangeEventArgs eventArgs)
-        {
-            Username = eventArgs.Value.ToString();
-        }
+        //protected void HandleUserNameChanged(ChangeEventArgs eventArgs)
+        //{
+        //    Username = eventArgs.Value.ToString();
+        //}
 
-        protected void HandleUserNameValueChanged(string value)
-        {
-            Username = value;
-        }
+        //protected void HandleUserNameValueChanged(string value)
+        //{
+        //    Username = value;
+        //}
 
-        public string GetError(Expression<Func<object>> fu)
-        {
-            if (EditContext == null)
-            {
-                return null;
-            }
-            return EditContext.GetValidationMessages(fu).FirstOrDefault();
-        }
+        //public string GetError(Expression<Func<object>> fu)
+        //{
+        //    if (EditContext == null)
+        //    {
+        //        return null;
+        //    }
+        //    return EditContext.GetValidationMessages(fu).FirstOrDefault();
+        //}
     }
 }
 
