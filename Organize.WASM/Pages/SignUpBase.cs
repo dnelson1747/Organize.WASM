cac315fb83ce327ce048aa0626e01b8406fd7ffa
dnelson1747 <dnelson1747@gmail.com>
2021-03-19 15:29:16 -0700

    .44

--- Organize.WASM/Pages/SignUpBase.cs
+++ Organize.WASM/Pages/SignUpBase.cs
@@ -1,4 +1,6 @@
-﻿using System;
+﻿using GeneralUI.DropdownControl;
+using Organize.Shared.Enums;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
@@ -7,5 +9,42 @@ namespace Organize.WASM.Pages
 {
     public class SignUpBase : SignBase
     {
+        protected IList<DropdownItem<GenderTypeEnum>> GenderTypeDropdownItems { get; } = new List<DropdownItem<GenderTypeEnum>>();
+
+        protected DropdownItem<GenderTypeEnum> SelectedGenderTypedropdownItem { get; set; }
+
+        protected override void OnInitialized()
+        {
+            base.OnInitialized(); 
+            {
+                base.OnInitialized();
+
+                var male = new DropdownItem<GenderTypeEnum>
+                {
+                    ItemObject = GenderTypeEnum.Male,
+                    DisplayText = "Male"
+                };
+
+                var female = new DropdownItem<GenderTypeEnum>
+                {
+                    ItemObject = GenderTypeEnum.Female,
+                    DisplayText = "Female"
+                };
+
+                var neutral = new DropdownItem<GenderTypeEnum>
+                {
+                    ItemObject = GenderTypeEnum.Neutral,
+                    DisplayText = "Others"
+                };
+
+                GenderTypeDropdownItems.Add(male);
+                GenderTypeDropdownItems.Add(female);
+                GenderTypeDropdownItems.Add(neutral);
+
+                SelectedGenderTypedropdownItem = female;
+            
+            }
+
+        }
     }
 }
